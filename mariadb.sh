338ae2a23483919631b4fc596db1fd2e8fa59970
Izzur <13365626+Izzur@users.noreply.github.com>
2025-05-01 16:21:32 +0700

    Update container image tag

--- mariadb.sh
+++ mariadb.sh
@@ -5,4 +5,4 @@ sudo podman run --name mariadb \
   -e MARIADB_USER=$DEFAULT_USER \
   -e MARIADB_PASSWORD=$DEFAULT_PASS \
   -e MARIADB_DATABASE=common \
-  -p 3306:3306 --rm docker.io/rapidfort/mariadb
+  -p 3306:3306 --rm docker.io/library/mariadb:latest
--- redis.sh
+++ redis.sh
@@ -3,6 +3,6 @@
 podman run --name redis \
   -e REDIS_PASSWORD=$DEFAULT_PASS \
   -p 6379:6379 \
-  --rm docker.io/rapidfort/redis
+  --rm docker.io/valkey/valkey:latest
 
 #  --network olympus \
